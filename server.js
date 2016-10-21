c5a79881621b84173075dd8f37717056e540d0d4
Jef King <jefkin@microsoft.com>
2016-10-21 06:14:49 -0600

    mocking data

--- server.js
+++ server.js
@@ -14,8 +14,32 @@ var expressPort = process.env.PORT || 8080;
 var router = express.Router();
 
 router.get('/', function (req, res) {
-	res.type('text/plain');
-	res.send("Nothing to see here...");
+
+	var mock = {
+		lat: 51.045091,
+		long: -114.057198,
+		id: 123,
+		content: 'This was my tweet #help.',
+		social: 'twitter',
+		profilepic: 'https://avatars0.githubusercontent.com/u/1208574?v=3&s=466',
+	};
+
+	res.jsonp(mock);
+});
+router.get('/:id', function (req, res) {
+
+	var id = parseInt(req.params.id);
+
+	var mock = {
+		lat: 51.045091,
+		long: -114.057198,
+		id: id,
+		content: 'This was my tweet #help.',
+		social: 'twitter',
+		profilepic: 'https://avatars0.githubusercontent.com/u/1208574?v=3&s=466',
+	};
+
+	res.jsonp(mock);
 });
 
 app.use('/', router);
