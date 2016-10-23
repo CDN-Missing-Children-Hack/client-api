48a27d4923c84ee9550d25a90e49611eac87667c
Mickey MacDonald <mimacdo@microsoft.com>
2016-10-23 13:03:12 -0400

    Added more depth to login endpoint

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,13 +1,30 @@
 // Load required packages
 var uuid = require('uuid');
-//var User = require('../models/user');
+//var User = require('./models/user');
 
+var user = ({
+    username: String,
+    password: String,
+    lastname: String,
+    firstname: String,
+    token: String
+});
+
+user.username = 'admin';
+user.password = 'password';
+user.firstname = 'Bob';
+user.lastname = 'Brady';
+user.token = uuid.v1();
 // Create endpoint /api/login for POST
 exports.postLogin = function(req, res) {
-    if(req.body.username == "admin" &&
-        req.body.password == "password"
+    if(req.body.username == user.username &&
+        req.body.password == user.password
       ) {
-            res.json({key: uuid.v1()});
+            res.json({
+                username : user.username,
+                firstname: user.firstname,
+                lastname: user.lastname,
+                token: user.token});
         }
         else{
             res.status(403).send({errorCode:"No Auth"});
