fbe824a6cf8b5de7bea1e18b6aa63b0eb7f7b013
Mickey MacDonald <mimacdo@microsoft.com>
2016-10-23 11:36:28 -0400

    Port change

--- src/server.js
+++ src/server.js
@@ -2,20 +2,13 @@ var express = require('express');
 var bodyParser = require("body-parser");
 var appInsights = require("applicationinsights");
 var DocumentDBClient = require('documentdb').DocumentClient;
-<<<<<<< HEAD
 var caseController = require('../src/controllers/case_controller');
 var userController = require('../src/controllers/user_controller');
 //Connect Application Insights
 var config = require('./config');
 appInsights.setup("config.appInsightsKey").start();
 //appInsights.setup("config.localkey").start();
-=======
-var config = require('./config');
-var controller = require('./controllers/case_controller');
 
-//Connect Application Insights
-appInsights.setup(config.appInsightsKey).start();
->>>>>>> 6a0e358e85d07062131315944d870f1e318ab050
 
 var app = express();
 
@@ -23,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //Set listening port
-var expressPort = process.env.PORT || 80;
+var expressPort = process.env.PORT || 8080;
 
 var router = express.Router();
 
